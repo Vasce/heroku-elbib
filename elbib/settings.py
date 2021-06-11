1820f9109515333262bd2690285380fd9c2e2449
Vasce <vas.viktory.016@gmail.com>
2021-06-11 14:11:17 +0300

    gitignore

--- elbib/settings.py
+++ elbib/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'elbib.wsgi.application'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
